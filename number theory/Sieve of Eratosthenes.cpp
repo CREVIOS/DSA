9375fc68b7f18f7c69a5e05496f66cb5b6c618d5
CREVIOS <48938983+CREVIOS@users.noreply.github.com>
2022-10-09 11:35:56 +0600

    Update Sieve of Eratosthenes.cpp

--- number theory/Sieve of Eratosthenes.cpp
+++ number theory/Sieve of Eratosthenes.cpp
@@ -23,9 +23,10 @@ for(int i= 2; i*i <n;i++)
 
 }
 
+
 int main()
 {
-    int n = 1e5+7;
+    int n = 1e6+7;
     sieve(n);
     int q;
     cin>>q;
@@ -37,3 +38,49 @@ int main()
     }
     return 0;
 }
+
+
+/* Optimization 1.0
+
+int sieve(int n)
+{
+
+prime[0] = prime[1] =false;
+
+for(int i= 3; i*i <n;i= i+2)
+{
+
+    if(prime[i]==true)
+    {
+    for(int j  = i*i ; j < n; j += i)
+    {
+        prime[j] = false;
+    }
+    }
+
+}
+
+}
+
+
+int main()
+{
+    int n = 1e6+7;
+    sieve(n);
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        int a;
+        cin>>a;
+        if(a%2==0 && a!=2)
+        {
+        cout<<0<<endl;
+        }
+        else{
+        cout<<prime[a]<<endl;
+        }
+    }
+    return 0;
+}
+*/
